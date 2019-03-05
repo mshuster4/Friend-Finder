df16e8b86ab813074ffa0e64049a067e025ee2f8
Emroze <emrozesd@users.noreply.github.com>
2019-03-04 18:20:51 -0800

    wrote friend compare logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,17 +11,80 @@ module.exports = function (app) {
 
         var newfriend = req.body;
 
-        console.log(newfriend);
+        var newfriendScores = req.body.scores;
 
-        friendsData.push(newfriend);
+        var totalDifferenceArr = [];
+
+        var bestFriendObj = {};
+
+        for (var i = 0; i < newfriendScores.length; i++) {
+            
+            newfriendScores[i] = parseInt(newfriendScores[i]);
+
+        }
 
         for (var i = 0; i < friendsData.length; i++) {
 
-            console.log(friendsData[i].scores);
+            var differenceArr = [];
+
+            var differenceNumber = 0;
+
+            var scoreDifference = 0;
+
+            var compFriend = friendsData[i];
+
+            for (var j = 0; j < compFriend.scores.length; j++) {
+
+                scoreDifference = Math.abs(parseInt(compFriend.scores[j]) - newfriendScores[j]);
+                differenceArr.push(scoreDifference);
+    
+            }
+
+            console.log(differenceArr);
+
+            for (var k = 0; k < differenceArr.length; k++) {
+
+                differenceNumber += differenceArr[k];
+
+            }
+
+            console.log("total difference: " + differenceNumber);
+            totalDifferenceArr.push(differenceNumber); 
+
         }
 
-        res.json(newfriend); 
+        console.log(totalDifferenceArr);
+
+        var compFriendNum = totalDifferenceArr[0];
+
+        var compFriendIndex = 0;
+
+        for (var i = 1; i < totalDifferenceArr.length; i++) {
+
+            if (totalDifferenceArr[i] < compFriendNum) {
+
+                compFriendNum = totalDifferenceArr[i];
+                compFriendIndex = i;
+
+            }
+        }
+
+        bestFriendObj = {
+            name: friendsData[compFriendIndex].name,
+            photo: friendsData[compFriendIndex].photo
+        };
+
+        console.log("index: " + compFriendIndex);
+
+        console.log("num: " + compFriendNum);
+
+        console.log("Best Friend is: " + bestFriendObj.name + bestFriendObj.photo);
+
+        friendsData.push(newfriend);
+
+        res.json(bestFriendObj); 
         
 
-    })
+    });
+
 }
